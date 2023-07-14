03d63086bb26d3cbc4667bd2032e58cb60665015
Gonzavidal <gvidal1992@gmail.com>
2023-07-14 23:58:38 +0000

    Prueba

---
+++ src/boatContainer.js
@@ -0,0 +1,28 @@
+import Boat from "./boat";
+
+function boatContainer(props) {
+  const { type, boxes, handleClick } = props;
+
+  const boxElements = [];
+  boxes.forEach((col, i) => {
+    col.forEach((box, j) => {
+      boxElements.push(
+        <Boat
+          onClickBox={() => handleClick(type, i, j)}
+          key={`${i}${j}`}
+          type={type}
+          boxValue={box}
+        />
+      );
+    });
+  });
+
+  return (
+    <div className="player-board">
+      <h1>{type}</h1>
+      <div className="box-container">{boxElements}</div>
+    </div>
+  );
+}
+
+export default boatContainer;
